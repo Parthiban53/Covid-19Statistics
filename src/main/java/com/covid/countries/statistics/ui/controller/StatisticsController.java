d9019e8158c8ee99bb03307f68e1b3e91f68f7b9
Parthiban53 <103716446+Parthiban53@users.noreply.github.com>
2022-04-19 08:42:02 +0530

    removed intstream range used

--- src/main/java/com/covid/countries/statistics/ui/controller/StatisticsController.java
+++ src/main/java/com/covid/countries/statistics/ui/controller/StatisticsController.java
@@ -1,10 +1,6 @@
 package com.covid.countries.statistics.ui.controller;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,14 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.covid.countries.statistics.service.StatisticsService;
 import com.covid.countries.statistics.ui.model.response.Covid19Stats;
-import com.covid.countries.statistics.ui.model.response.StatisticsResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 @RestController
 public class StatisticsController {
@@ -32,9 +20,9 @@ public class StatisticsController {
 	@Autowired
 	StatisticsService statsService;
 
-	@GetMapping(value = "/getCovidStatisticsReportForAllCountries", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Covid19Stats>> getCovidStatisticsReportForAllCountries() throws Exception {
-		List<Covid19Stats> covid19Stats = getAPIData(Util.BASE_URL);
+	@GetMapping(value = "/getStatisticsForAll", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Covid19Stats>> getStatisticsForAll() throws Exception {
+		List<Covid19Stats> covid19Stats = Util.getAPIData(Util.BASE_URL);
 		if (null != covid19Stats) {
 			statsService.saveStatistics(covid19Stats);
 			return new ResponseEntity<List<Covid19Stats>>(covid19Stats, HttpStatus.OK);
@@ -43,55 +31,30 @@ public class StatisticsController {
 		}
 	}
 
-	@GetMapping(value = "/getCovidStatisticsReportForSpecificCountry/{country}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Covid19Stats>> getCovidStatisticsReportForSpecificCountry(@PathVariable("country") String country,
+	@GetMapping(value = "/getStatisticsForCountry/{country}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Covid19Stats>> getStatisticsForCountry(@PathVariable("country") String country,
 			@RequestParam(value = "pageNumber", required = true) int pageNumber,
 			@RequestParam(value = "pageSize", required = true) int pageSize) throws Exception {
 		String url = Util.BASE_URL + "?country=" + country;
-		List<Covid19Stats> covid19Stats = getAPIData(url);
-		if (null != covid19Stats) {
-			Map<Integer, List<Covid19Stats>> covid19StatsMap = IntStream
-					.range(0, (covid19Stats.size() + pageSize - 1) / pageSize).boxed()
-					.collect(Collectors.toMap(i -> i, i -> covid19Stats.subList(i * pageSize,
-							Math.min((i + 1) * pageSize, covid19Stats.size()))));
-			List<Covid19Stats> filteredCovid19Stats = (null != covid19StatsMap)
-					? null != covid19StatsMap.get(pageNumber-1) ? covid19StatsMap.get(pageNumber-1) : null
-					: null;
-			if (null != filteredCovid19Stats) {
-				return new ResponseEntity<List<Covid19Stats>>(filteredCovid19Stats, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<List<Covid19Stats>>(HttpStatus.NO_CONTENT);
-			}
+		List<Covid19Stats> covid19Stats = Util.getAPIData(url);
+		List<Covid19Stats> filteredCovid19Stats = covid19Stats != null
+				? covid19Stats.subList((pageNumber-1) * pageSize,
+						Math.min(((pageNumber-1) + 1) * pageSize, covid19Stats.size()))
+				: null;
+		if (null != filteredCovid19Stats) {
+			return new ResponseEntity<List<Covid19Stats>>(filteredCovid19Stats, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<List<Covid19Stats>>(HttpStatus.NO_CONTENT);
 		}
 	}
 
-	@GetMapping(value = "/getCovidStatisticsReportForAllCountriesFromH2DB", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Covid19Stats>> getCovidStatisticsReportForAllCountriesFromH2DB() throws Exception {
+	@GetMapping(value = "/getStatisticsFromH2", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Covid19Stats>> getStatisticsFromH2() throws Exception {
 		List<Covid19Stats> covid19Stats = statsService.getStatistics();
 		if (null != covid19Stats) {
 			return new ResponseEntity<List<Covid19Stats>>(covid19Stats, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<List<Covid19Stats>>(HttpStatus.NO_CONTENT);
 		}
-	}
-
-	private List<Covid19Stats> getAPIData(String url)
-			throws IOException, JsonProcessingException, JsonMappingException {
-		OkHttpClient client = new OkHttpClient();
-		Request request = new Request.Builder().url(url).get()
-				.addHeader("X-RapidAPI-Host", "covid-19-coronavirus-statistics.p.rapidapi.com")
-				.addHeader("X-RapidAPI-Key", "b5b7b30590msh9ae848af01a7cfdp1d03d4jsn0d4fe5f504e6").build();
-		Response response = client.newCall(request).execute();
-		if (!response.isSuccessful())
-			throw new IOException("Unexpected code " + response);
-		String rawResponse = response.body().string();
-		List<Covid19Stats> covid19Stats = null;
-		if (null != rawResponse && !rawResponse.isEmpty()) {
-			StatisticsResponse sResponse = new ObjectMapper().readValue(rawResponse, StatisticsResponse.class);
-			covid19Stats = sResponse.getData() != null ? sResponse.getData().getCovid19Stats() : null;
-		}
-		return covid19Stats;
 	}
 }
--- src/main/java/com/covid/countries/statistics/ui/controller/Util.java
+++ src/main/java/com/covid/countries/statistics/ui/controller/Util.java
@@ -1,7 +1,38 @@
 package com.covid.countries.statistics.ui.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.covid.countries.statistics.ui.model.response.Covid19Stats;
+import com.covid.countries.statistics.ui.model.response.StatisticsResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Util {
 	
 	public static final String BASE_URL = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats";
+	
+	public static List<Covid19Stats> getAPIData(String url)
+			throws IOException, JsonProcessingException, JsonMappingException {
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url(url).get()
+				.addHeader("X-RapidAPI-Host", "covid-19-coronavirus-statistics.p.rapidapi.com")
+				.addHeader("X-RapidAPI-Key", "b5b7b30590msh9ae848af01a7cfdp1d03d4jsn0d4fe5f504e6").build();
+		Response response = client.newCall(request).execute();
+		if (!response.isSuccessful())
+			throw new IOException("Unexpected code " + response);
+		String rawResponse = response.body().string();
+		List<Covid19Stats> covid19Stats = null;
+		if (null != rawResponse && !rawResponse.isEmpty()) {
+			StatisticsResponse sResponse = new ObjectMapper().readValue(rawResponse, StatisticsResponse.class);
+			covid19Stats = sResponse.getData() != null ? sResponse.getData().getCovid19Stats() : null;
+		}
+		return covid19Stats;
+	}
 
 }
